df123c991fcebf50daa21c1c7d1e4e72c93ff72d
yevgeniy007 <yevgeniy.x.samyshkin@gsk.com>
2015-04-12 17:32:19 +0100

    Plot 1 - Histogram - pre-final
    Plot 1 - Histogram - pre-final

--- Plot_1.R
+++ Plot_1.R
@@ -1,65 +1,56 @@
 # ===================================================================================
 # Course Project 1 - Exploratory Data Analysis
 # Student Yevgeniy Samuyshkin
+# ===================================================================================
+# At the end of this script there is a short discussion / interpretation of the histogram 
 
 
+# Clean memory from any garbage 
 rm(list=ls())
 
-# Your working directory should contain txt datafile and code 
-
-setwd("C:/Users/ys906826/Documents/04. Training/07. Coursera III (Exploratory Analysis)/Course Project 1")
-
-# Read data file, it is a table with 9 columns (variables) separated by semicolon
-# Variable names (column headings) are in the 1st row of the table 
-
-# Check memory here programmatically - outstanding 
+# The current working directory should contain both txt datafile and code, no subdirectories  
 
 library(lubridate)
 library(base)
 library(dplyr)
 library(grDevices)
 
+# My laptop has large memory - I do not need to worry whether it has sufficient memory to updload file - I have tested that by uploadig the file into several different datasets
 mydata<-read.table("household_power_consumption.txt", header=TRUE, sep = ";")
 
-# We will only be using data from the dates 2007-02-01 and 2007-02-02
-	
-mydata1<-select(mydata, Date, Time, Global_active_power, Global_reactive_power, Sub_metering_1, Sub_metering_2, Sub_metering_3)
+mydata<-select(mydata, Date, Time, Global_active_power)
 
+mydata$Date1 <- as.Date(mydata$Date,"%d/%m/%Y")
+### mydata$Time1 <- as.character(mydata$Time)
 
+### This is for filtering: 
+mydata$Year<-year(mydata$Date1)
+mydata$Month<-month(mydata$Date1)
+mydata$Day<-day(mydata$Date1)
 
-mydata1$Date1 <- as.Date(mydata1$Date,"%d/%m/%Y")
-mydata1$Time1 <- as.character(mydata1$Time)
+mydata<-filter(mydata, Year==2007 & Month==2 & Day<=2)
 
-rm(mydata)
+## Active power needs to be numeric for histogram 
+mydata$Global_active_power<-as.numeric(as.character(mydata$Global_active_power))
 
-mydata1$Year<-year(mydata1$Date1)
-mydata1$Month<-month(mydata1$Date1)
-mydata1$Day<-day(mydata1$Date1)
+## ============== Histogram 1
 
-# Selected data for charting 
-mydata2<-filter(mydata1, Year==2007 & Month==2 & Day<=2)
-
-mydata2$Time2 <- strptime(mydata2$Time1, format="%H:%M")
-
-rm(mydata1)
-
-mydata2$Global_active_power<-as.numeric(as.character(mydata2$Global_active_power))
-mydata2$Global_reactive_power<-as.numeric(as.character(mydata2$Global_reactive_power))
-mydata2$Sub_metering_1<-as.numeric(as.character(mydata2$Sub_metering_1))
-mydata2$Sub_metering_2<-as.numeric(as.character(mydata2$Sub_metering_2))
-mydata2$Sub_metering_3<-as.numeric(as.character(mydata2$Sub_metering_3))
-
-## Histogram 1
-
+# Set output device as PNG and determine the filename and size in pixels
 png(filename = "Myplot1.png", width = 480, height = 480, units = "px")
-hist(mydata2$Global_active_power, ylab="Frequency", xlab="Global Active Power (kilowatts)", col="red", breaks=20, main="Global Active Power")
-## dev.copy(png, file = "cgrap1.png")
+hist(mydata$Global_active_power, ylab="Frequency", xlab="Global Active Power (kilowatts)", col="red", breaks=20, main="Global Active Power")
 
+## Close the file on completon 
 dev.off()
 
 
-# rm(mydata)
+## === Interpetation ============================================
+## What does this histogram tells us? - a hypothesis below
+## Most frequetl observations correspond to low values of power per minute, probably corresponding to lightig  (we cannot tell this from data but can explore further)
+## A peak in the histogram Bar 3 is likely due to a typical power consumptiopn of household appliances, representing a threshold
+## if we produce this histogram with a higher resolution (Histogram-(scenario)-higher resolution.png), we can see clearly a discrete nature of power consumption 
 
+
+# === End of script 
  
 
 
